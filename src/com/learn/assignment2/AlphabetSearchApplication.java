5978e131a3835e1c0cb84c4afe5064cfed1b393f
Naveen S <naveen.sandyapaga@zemosolabs.com>
2020-12-14 17:55:39 +0530

    assignment-2

---
+++ src/com/learn/assignment2/AlphabetSearchApplication.java
@@ -0,0 +1,43 @@
+package com.learn.assignment2;
+
+
+
+/* Problem: Write a java function that checks if the input string contains all the letters of the alphabet a-z
+(case-insensitive). Write time and space complexity of your solution as comments in the source file.
+ Time Complexity: O(n)
+ Space Complexity: O(1)
+ */
+
+public class AlphabetSearchApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println(containsAllAlphabets("abcdefghijklmadsafsdfdfnopqrstuvwxyZ"));
+        System.out.println(containsAllAlphabets("naveen"));
+    }
+
+    public static boolean containsAllAlphabets(String input) {
+
+        int[] charCount = new int[26];
+
+        if (input == null || input.isEmpty())
+            return false;
+
+        input = input.toLowerCase();
+        char[] chars = input.toCharArray();
+
+        if (chars.length < 26)
+            return false;
+
+        for(char ch: chars) {
+            int index = ch - 'a';
+            charCount[index] = 1;
+        }
+
+        for (int count: charCount) {
+            if (count == 0)
+                return false;
+        }
+        return true;
+    }
+}
